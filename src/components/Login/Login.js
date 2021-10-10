f252a44a3017e459bcfabce8bce703b423db4209
sabbir0029 <mdsabbirhossain0029@gmail.com>
2021-10-10 20:33:11 +0600

    new add

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>
-      <h1>login</h1>
+    <div className='login'>
+      <div>
+        <h1>Login</h1>
+        <form>
+        <input type="user" />
+        <br />
+        <input type="password"/>
+        <br />
+        <input type="submit" value="Submit"/>
+        </form>
+        <p>New to E Bazaar? <Link to='ragister'>Create Account</Link></p>
+        <div>...........Or.............</div>
+        <button className='btn-regular'>Google login</button>
+      </div>
     </div>
   );
 };
---
+++ src/components/firebase/firebaseConfig .js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9l3ddEkimwnIGTcr_hdvO-1YLW-MxAnA",
+  authDomain: "e-bazaar-4090a.firebaseapp.com",
+  projectId: "e-bazaar-4090a",
+  storageBucket: "e-bazaar-4090a.appspot.com",
+  messagingSenderId: "776806325016",
+  appId: "1:776806325016:web:4853120a4650c8a7581c6d"
+};
+
+export default firebaseConfig;
